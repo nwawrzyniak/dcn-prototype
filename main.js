8f7e4c7913dcc4ccb6c114bb4eaf0d27670bfb23
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-04-19 20:58:03 +0200

    Added toggle, fixed scaling a bit

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 function onLoad() {
     const tableObject = document.querySelector("img");
     const screenTeensBox = document.querySelector("#screen_teens_box");
+    var toggle=true;
 
     tableObject.addEventListener("click", (event) => {
-        //document.body.style.backgroundColor = "black";
-        screenTeensBox.style.display = "block";
+        if (toggle) {
+            screenTeensBox.style.display = "block";
+        } else {
+            screenTeensBox.style.display = "none";
+        }
+        toggle = !toggle;
       });
 }
 
