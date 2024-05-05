c4d9ac811f5b0dcdf02eaea7f7803a2a185e48c6
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-05-05 18:43:52 +0200

    Refactored

--- main.js
+++ main.js
@@ -46,23 +46,36 @@ function onLoad() {
     circle5.addEventListener('click', handleOverlay(overlay5));
     // Für weiteren Kreis mit zugehörigem Overlay die obere Zeile kopieren und die Zahl an beiden Stellen um 1 erhöhen
 
-    const rect = deskImage.getBoundingClientRect();
-    const width = rect.width;
-    const height = rect.height;
+    deskImage.addEventListener('click', deactivateOverlays()); // Diese Zeile auskommentieren, damit ein Klick auf eine freie Fläche im Bild NICHT das aktuell geöffnete Overlay schließt
+
+    const circle1X = 0.47;
+    const circle1Y = 0.8;
+    const circle2X = 0.88;
+    const circle2Y = 0.63;
+    const circle3X = 0.1;
+    const circle3Y = 0.7;
+    const circle4X = 0.2;
+    const circle4Y = 0.15;
+    const circle5X = 0.9;
+    const circle5Y = 0.1;
+    // Für weiteren Kreis die zwei Zeilen über dieser kopieren, die Zahl im Namen um 1 erhöhen und die neuen Positionen angeben
+
+    const overlay1X = 0.55;
+    const overlay1Y = 0.5;
+    const overlay2X = 0.57;
+    const overlay2Y = 0.8;
+    const overlay3X = 0.18;
+    const overlay3Y = 0.6;
+    const overlay4X = 0.2;
+    const overlay4Y = 0.3;
+    const overlay5X = 0.75;
+    const overlay5Y = 0.25;
+    // Für weiteres Overlay die zwei Zeilen über dieser kopieren, die Zahl im Namen um 1 erhöhen und die neuen Positionen angeben
 
     function positionCircles() {
-        const circle1X = 0.47;
-        const circle1Y = 0.8;
-        const circle2X = 0.88;
-        const circle2Y = 0.63;
-        const circle3X = 0.1;
-        const circle3Y = 0.7;
-        const circle4X = 0.2;
-        const circle4Y = 0.15;
-        const circle5X = 0.9;
-        const circle5Y = 0.1;
-        // Für weiteren Kreis die zwei Zeilen über dieser kopieren, die Zahl im Namen um 1 erhöhen und die neuen Positionen angeben
-
+        const rect = deskImage.getBoundingClientRect();
+        const width = rect.width;
+        const height = rect.height;
         circle1.style.left = width * circle1X + rect.left + 'px';
         circle1.style.top = height * circle1Y + 'px';
         circle2.style.left = width * circle2X + rect.left + 'px';
@@ -77,18 +90,9 @@ function onLoad() {
     }
 
     function positionOverlays() {
-        const overlay1X = 0.55;
-        const overlay1Y = 0.5;
-        const overlay2X = 0.57;
-        const overlay2Y = 0.8;
-        const overlay3X = 0.18;
-        const overlay3Y = 0.6;
-        const overlay4X = 0.2;
-        const overlay4Y = 0.3;
-        const overlay5X = 0.75;
-        const overlay5Y = 0.25;
-        // Für weiteres Overlay die zwei Zeilen über dieser kopieren, die Zahl im Namen um 1 erhöhen und die neuen Positionen angeben
-
+        const rect = deskImage.getBoundingClientRect();
+        const width = rect.width;
+        const height = rect.height;
         overlay1.style.left = width * overlay1X + rect.left + 'px';
         overlay1.style.top = height * overlay1Y + 'px';
         overlay2.style.left = width * overlay2X + rect.left + 'px';
@@ -102,10 +106,17 @@ function onLoad() {
         // Für weiteres Overlay die oberen zwei Zeilen kopieren und die Zahl an beiden Stellen um 1 erhöhen
     }
 
-    window.addEventListener("resize", positionCircles);
-    window.addEventListener("resize", positionOverlays);
-    positionCircles();
-    positionOverlays();
+    function positionCirclesAndOverlays() {
+        positionCircles();
+        positionOverlays();
+    }
+
+    window.addEventListener("resize", positionCirclesAndOverlays);
+    positionCirclesAndOverlays();
 }
 
-document.addEventListener("DOMContentLoaded", onLoad);
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", onLoad);
+} else {
+    onLoad();
+}
